aa62b37a7b0256d35215ba4057234d23f7c2df26
candyflossuk <ross.a.humphrey@googlemail.com>
2019-10-09 13:40:56 +0100

    Added notes about composition vs inheritence

---
+++ notes/general/composition-vs-inheritence.js
@@ -0,0 +1,11 @@
+// Instead of thinking in terms of what things are, what if we think in terms
+// of what they do?
+
+// Use Object.assign to merge the state of an objects with some methods
+
+// By favoring composition over inheritence and thinking
+// about what objects do rather than what things are, we are free of fragile
+// and tightly coupled inheritence structures.
+
+// This is also known as the functional instantiation pattern, not involving
+// the prototype pattern at all.
