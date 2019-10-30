939050d35745f296261b50611b47dae80eb2ce2e
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-10-30 08:24:33 +0000

    Added state to navbar and also split it into it's own component

--- notes/project/github-battle/app/components/Popular.js
+++ notes/project/github-battle/app/components/Popular.js
@@ -1,20 +1,54 @@
 import React from 'react'
 
+function LanguagesNav ({ selected, onUpdateLanguage }){
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+
+  return (
+    <ul className='flex-center'>
+      {languages.map((language) => (
+        <li key={language}>
+          <button className='btn-clear nav-link'
+          style={language === selected ? {color: 'rgb(187, 46, 31)'} : null}
+          onClick={()=> onUpdateLanguage(language)}>
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 export default class Popular extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    // Ensures that the 'this' context is always that of Popular - not where this is used in a function (for example)
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage(selectedLanguage){
+    // Always use setState here - setState lets React know we have changed state and cause re render
+    this.setState({
+      selectedLanguage
+    })
+  }
+
   render(){
-
-    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
-
+    const { selectedLanguage } = this.state
     return (
-      <ul className='flex-center'>
-        {languages.map((language) => (
-          <li key={language}>
-            <button className='btn-clear nav-link'>
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </React.Fragment>
     )
   }
 }
+
+// Using ()=> Arrow function, it gives react a 'pointer' to the function, so whenever clicked invoke this function
