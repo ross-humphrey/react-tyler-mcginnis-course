520de3e5420b49f44bd9f6bef7bd431bd6d15427
candyflossuk <ross.a.humphrey@googlemail.com>
2019-11-05 13:00:53 +0000

    Added react icons file - with how to use the icons

---
+++ notes/the-component-lifecycle/react-icons.js
@@ -0,0 +1,19 @@
+// Icons are key part of a UI
+// Use react-icons from NPM
+// react-icons give us access to all the icons available in Font Awesome, Ionicons, Material Design,
+// Github Octicons + more!
+
+// Font Awesome
+import { FaIconName } from 'react-icons/fa'
+
+// Material Design
+import { MdIconName } from 'react-icons/md'
+
+// Ionicons
+import { IOIconName } from 'reaction-icons/io'
+
+// Github Octicons
+import { GoIconName } from 'react-icons/go'
+
+// Customizing
+// 3 props to customize are size, color and className
