60b05e4aec56a36b7749f297e87faaf19f26a1b2
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-11-12 16:33:46 +0000

    Added component for playerone and playertwo entry to the battle page

--- notes/project/github-battle/app/components/Battle.js
+++ notes/project/github-battle/app/components/Battle.js
@@ -83,10 +83,47 @@ PlayerInput.propTypes = {
 }
 
 export default class Battle extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      playerOne : null,
+      playerTwo: null,
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(id, player){
+    this.setState({
+      [id]: player
+    })
+  }
+
   render() {
+
+    const { playerOne, playerTwo } = this.state
+
     return(
       <React.Fragment>
         <Instructions />
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {playerOne === null && (
+              <PlayerInput
+                label='PlayerOne'
+                onSubmit={(player) => this.handleSubmit('playerOne', player )}
+              />
+            )}
+            {playerTwo === null && (
+              <PlayerInput
+                label='playerTwo'
+                onSubmit={(player) => this.handleSubmit('playerTwo', player )}
+              />
+            )}
+          </div>
+        </div>
       </React.Fragment>
     )
   }
