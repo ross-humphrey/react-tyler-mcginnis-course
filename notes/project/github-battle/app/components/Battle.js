1be3c1bcc4d47b383333323eb9b3d3ce827f1749
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-11-15 07:12:02 +0000

    Added battle component to the project that is shown when two usernames are entered

--- notes/project/github-battle/app/components/Battle.js
+++ notes/project/github-battle/app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
+import Results from './Results'
 
 function Instructions(){
   return(
@@ -121,6 +122,7 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne : null,
       playerTwo: null,
+      battle: false
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleReset = this.handleReset.bind(this)
@@ -140,7 +142,11 @@ export default class Battle extends React.Component {
 
   render() {
 
-    const { playerOne, playerTwo } = this.state
+    const { playerOne, playerTwo, battle } = this.state
+
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo}/>
+    }
 
     return(
       <React.Fragment>
@@ -171,6 +177,14 @@ export default class Battle extends React.Component {
                   />
               }
           </div>
+          {playerOne && playerTwo && (
+            <button
+              className='btn dark-btn btn-space'
+              onClick={() => this.setState({battle: true})}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     )
