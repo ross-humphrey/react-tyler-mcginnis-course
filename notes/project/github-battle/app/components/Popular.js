146299d058c8706ac969711a841b14ca9051d4c9
Ross Humphrey <ross.a.humphrey@googlemail.com>
2019-11-23 09:30:29 +0000

    Added winner and loser cards to the results page after battle has taken place

--- notes/project/github-battle/app/components/Popular.js
+++ notes/project/github-battle/app/components/Popular.js
@@ -34,7 +34,7 @@ function ReposGrid ({ repos }) {
         const { login, avatar_url } = owner
 
         return (
-          <li key ={html_url} className='repo bg-light'>
+          <li key ={html_url} className='card bg-light'>
             <h4 className='header-lg center-text'>
               #{index +1}
             </h4>
@@ -141,7 +141,7 @@ export default class Popular extends React.Component {
           onUpdateLanguage={this.updateLanguage}
         />
         {this.isLoading() && <p>LOADING</p>}
-        {error && <p>{error}</p>}
+        {error && <p className='center-text error'>{error}</p>}
         {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]}/>}
       </React.Fragment>
     )
