032f02f35790c5ea6ded0cb2250ff83ee9e1cbfe
candyflossuk <ross.a.humphrey@googlemail.com>
2020-01-16 09:03:17 +0000

    Added a first cut of the Info using the higher order component - currently lacking the height property - so need to do further work

--- notes/code-sharing/higher-order-components.js
+++ notes/code-sharing/higher-order-components.js
@@ -187,7 +187,7 @@ function Info ({ hovering, height }) {
   )
 }
 
-function withHover(Component) {
+function withHover(Component, propName = 'hovering') { // By allowing invoker to specify name we prevent naming collisions
   return class WithHover extends React.Component {
     constructor(super){
       super(props)
@@ -206,11 +206,32 @@ function withHover(Component) {
       this.setState({hovering: false})
     }
     render(){
+      const props = {
+        [propName]: this.state.hovering
+      }
       return (
         <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
-          <Component hovering={this.state.hovering}
+          <Component {...props}/>
         </div>
       );
     }
   }
 }
+
+function Info({ showTooltip, height}) {
+  return (
+    <>
+      {showTooltip === true
+          ? <ToolTip id={this.props.id}/>
+          :null}
+      <svg
+        className="Icon-svg Icon--hoverable-svg"
+        height={height}
+        viewBox="0 0 16 16" width="16">
+          <path d="M9 8a1 1 0 0 0-1-1H5.5a1 1 0 1 0 0 2H7v4a1 1 0 0 0 2 0zM4 0h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4zm4 5.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+    </svg>
+    </>
+  )
+}
+
+const InfoWithHover = withHover(Info, 'showTooltip')
